4f33d038dd246b8f33ba4b0874a3e6cdf47b5e4d
VennyS <panarin.danil.08@gmail.co>
2025-04-20 13:43:33 +0500

    feat: created new class for errors

---
+++ internal/shared/errors.go
@@ -0,0 +1,23 @@
+package shared
+
+import (
+	"errors"
+	"net/http"
+)
+
+type HttpError struct {
+	Err        error
+	StatusCode int
+}
+
+func (h *HttpError) Error() string {
+	return h.Err.Error()
+}
+
+func NewHttpError(err string, statusCode int) *HttpError {
+	return &HttpError{Err: errors.New(err), StatusCode: statusCode}
+}
+
+var (
+	InternalError = NewHttpError("internal server error", http.StatusInternalServerError)
+)
