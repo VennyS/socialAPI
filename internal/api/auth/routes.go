85ccfb465caf406d701b6507f05ec1c3f13c8093
VennyS <panarin.danil.08@gmail.co>
2025-04-20 13:47:21 +0500

    created auth/login handler

---
+++ internal/api/auth/routes.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"socialAPI/internal/api"
+	"socialAPI/internal/api/service/auth"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type AuthController struct {
+	authService auth.AuthService
+}
+
+func NewAuthController(authService auth.AuthService) *AuthController {
+	return &AuthController{authService: authService}
+}
+
+func (a AuthController) RegisterRoutes(r *chi.Mux) {
+	r.Route("/auth", func(r chi.Router) {
+		r.With(api.JsonBodyMiddleware[auth.UserRequest]()).Post("/login", a.LoginHandler())
+	})
+}
