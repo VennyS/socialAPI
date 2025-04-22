bffd816ea6eeecbb5af7f0654b8bb52c06fa8c9d
VennyS <panarin.danil.08@gmail.com>
2025-04-22 18:24:52 +0500

    refactor: added param to exclude user if necessary

--- internal/storage/repository/user.go
+++ internal/storage/repository/user.go
@@ -1,11 +1,14 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type UserRepository interface {
 	Create(user *User) error
 	FindByEmail(email string) (*User, error)
 	Exists(email string) (bool, error)
+	GetAll(excludeID *uint) ([]User, error)
 }
 
 type userPostgresRepo struct {
@@ -30,6 +33,7 @@ func (repo userPostgresRepo) Create(user *User) error {
 	}
 	return nil
 }
+
 func (repo userPostgresRepo) Exists(email string) (bool, error) {
 	var count int64
 	err := repo.db.Model(&User{}).Where("email = ?", email).Count(&count).Error
@@ -37,4 +41,23 @@ func (repo userPostgresRepo) Exists(email string) (bool, error) {
 		return false, err
 	}
 	return count > 0, nil
+}
+
+func (repo userPostgresRepo) GetAll(excludeID *uint) ([]User, error) {
+	var users []User
+
+	// Если excludeID не nil, исключаем пользователя с этим id из выборки
+	if excludeID != nil {
+		err := repo.db.Where("id != ?", *excludeID).Find(&users).Error
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		err := repo.db.Find(&users).Error
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return users, nil
 }
