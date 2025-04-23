6485dd3a0f327d56a607e676c2c2b540ce8acf82
VennyS <panarin.danil.08@gmail.com>
2025-04-23 23:32:57 +0500

    refactor: add some settings to init logger

--- internal/shared/logger.go
+++ internal/shared/logger.go
@@ -5,11 +5,9 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-// InitLogger инициализирует логгер в зависимости от окружения
 func InitLogger(appEnv string) (*zap.SugaredLogger, error) {
 	var config zap.Config
 
-	// Настроим логгер для production и development
 	if appEnv == "production" {
 		config = zap.NewProductionConfig()
 		config.Level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
@@ -19,6 +17,15 @@ func InitLogger(appEnv string) (*zap.SugaredLogger, error) {
 		config.Encoding = "console"
 		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	}
+
+	config.DisableStacktrace = true
+	config.EncoderConfig.TimeKey = "time"
+	config.EncoderConfig.LevelKey = "level"
+	config.EncoderConfig.NameKey = "logger"
+	config.EncoderConfig.CallerKey = ""
+	config.EncoderConfig.MessageKey = "msg"
+	config.EncoderConfig.StacktraceKey = ""
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 
 	logger, err := config.Build()
 	if err != nil {
